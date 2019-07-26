f43476cf126244f16e3196f710cf07d3e46e7b6f
Dmitriy Boltovskiy <dboltovskyi@luxoft.com>
2019-07-26 16:09:57 -0400

    Add possibility to build SDL in container

--- sdl/entrypoint.sh
+++ sdl/entrypoint.sh
@@ -4,7 +4,7 @@ export HOME=/home/developer
 
 USER_ID=${LOCAL_USER_ID:-9001}
 SDL=$HOME/sdl
-HMI=$HOME/sdl/hmi
+HMI=$HOME/sdl/sdl_hmi
 
 echo "Configuring and starting web server"
 sed 's|\/var\/www\/html|'"$HMI"'|' -i /etc/lighttpd/lighttpd.conf
@@ -15,9 +15,11 @@ useradd -s /bin/bash -u $USER_ID -o -c "" -M developer
 echo "developer ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 chown developer $HOME
 chgrp developer $HOME
+chown developer $HOME/.zshrc
+chgrp developer $HOME/.zshrc
 
 echo "Running init script"
-sudo -E -u developer $SDL/scripts/init.sh
+sudo -E -u developer $SDL/init.sh
 
 echo "Executing command under 'developer' user: $@"
 sudo -E -u developer "$@"
